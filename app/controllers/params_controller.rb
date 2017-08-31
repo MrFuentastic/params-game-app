cb963379de5e0fd47068f1276080a7932cd6f4b9
MrFuentastic <vfuentes8587@gmail.com>
2017-08-30 19:19:45 -0500

    made two pages for games app

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -1,2 +1,10 @@
 class ParamsController < ApplicationController
+  def name_game_app
+    @name = params[:my_name]
+  end
+
+  def number_guessing_app
+    @answer = 42
+    @guess = params['guess'].to_i
+  end
 end
